7e0b6c642d703090a955d128a0676826cf6bb717
PixelMan <vitaliyksyu@gmail.com>
2017-12-15 15:05:30 +0200

    Added files

---
+++ Gruntfile.js
@@ -0,0 +1,172 @@
+module.exports = function(grunt) {
+  require('time-grunt')(grunt);
+  require('load-grunt-tasks')(grunt);
+
+
+  grunt.initConfig({
+    clean: {
+      build: ['build']
+    },
+    copy: {
+      build: {
+        files: [{
+          expand: true,
+          src: [
+            'css/normalize.css',
+            'fonts/**/*.{woff,woff2,eot,ttf,svg}',
+            'img/**',
+            'js/**',
+            '*.html'
+          ],
+          dest: 'build'
+        }]
+      },
+      html: {
+        files: [{
+          expand: true,
+          src: ['*.html'],
+          dest: 'build'
+        }]
+      },
+      js: {
+        files: [{
+          expand: true,
+          src: ['js/*.js'],
+          dest: 'build'
+        }]
+      }
+    },
+    less: {
+      style: {
+        files: {
+          'build/css/style.css': 'less/style.less'
+        }
+      }
+    },
+    postcss: {
+      style: {
+        options: {
+          processors: [
+            require('autoprefixer')({
+              browsers: [
+                'last 1 version',
+                'last 2 Chrome versions',
+                'last 2 Firefox versions',
+                'last 2 Opera versions',
+                'last 2 Edge versions'
+              ]
+            }),
+            require('css-mqpacker')({
+              sort: true
+            })
+          ]
+        },
+        src: 'build/css/*.css'
+      }
+    },
+    csso: {
+      style: {
+        options: {
+          report: 'gzip'
+        },
+        files: {
+          'build/css/style.min.css': ['build/css/style.css']
+        }
+      }
+    },
+    jshint: {
+      options: {
+        reporter: require('jshint-stylish')
+      },
+      main: [
+        'build/js/script.js'
+      ]
+    },
+    uglify: {
+      my_target: {
+        files: {
+          'build/js/script.min.js': ['build/js/script.js']
+        }
+      }
+    },
+    svgstore: {
+      options: {
+        svg: {
+          style: 'display: none'
+        }
+      },
+      symbols: {
+        files: {
+          'build/img/symbols.svg': ['img/icons/*.svg']
+        }
+      }
+    },
+    svgmin: {
+      symbols: {
+        files: [{
+          expand: true,
+          src: ['build/img/**/*.svg']
+        }]
+      }
+    },
+    imagemin: {
+      images: {
+        options: {
+          optimizationLevel: 3
+        },
+        files: [{
+          expand: true,
+          src: ['build/img/**/*.{png,jpg,gif}']
+        }]
+      }
+    },
+    browserSync: {
+      server: {
+        bsFiles: {
+          src: ['build/*.html', 'build/css/*.css']
+        },
+        options: {
+          server: 'build',
+          watchTask: true
+        }
+      }
+    },
+    watch: {
+      html: {
+        files: ['*.html'],
+        tasks: ['copy:html']
+      },
+      style: {
+        files: ['less/**/*.less'],
+        tasks: ['less', 'postcss', 'csso']
+      },
+      scripts: {
+        files: ['js/*.js'],
+        tasks: ['jshint', 'copy:js', 'uglify'],
+        options: {
+          interrupt: true,
+        }
+      },
+      configFiles: {
+        files: ['Gruntfile.js'],
+        options: {
+          reload: true
+        }
+      }
+    }
+  });
+
+  grunt.registerTask('serve', ['browserSync', 'watch']);
+  grunt.registerTask('symbols', ['svgmin', 'svgstore']);
+  grunt.registerTask('build', [
+    'clean',
+    'copy',
+    'less',
+    'postcss',
+    'csso',
+    'jshint',
+    'uglify',
+    'symbols',
+    'imagemin'
+  ]);
+};
